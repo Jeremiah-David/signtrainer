ad988ba87ade15335f74c72aecd9f8573bc544df
Jeremiah <dancesfire@gmail.com>
2021-05-07 11:18:14 -0700

    gesture rec working on two pre made signs

--- src/components/hand/index.js
+++ src/components/hand/index.js
@@ -1,9 +1,12 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import * as tf from "@tensorflow/tfjs"
 import * as handpose from "@tensorflow-models/handpose"
 import Webcam from "react-webcam"
 import '../../App.css'
-import {drawHand} from './utilties'
+import { drawHand } from './utilties'
+import * as fp from 'fingerpose'
+// import victory from '../../victory.png'
+// import thumbsup from '../../thumbsup.png'
 
 
 
@@ -13,6 +16,10 @@ function Hand() {
     // reference for using webcam and canvas in functions
     const webcamRef = useRef(null)
     const canvasRef = useRef(null)
+
+    //set state for gestures
+    const [recGesture, setRecGesture] = useState({})
+
 
     const runHandpose = async () => {
         const net = await handpose.load()
@@ -45,6 +52,24 @@ function Hand() {
             //make detections
             const hand = await net.estimateHands(video)
             console.log(hand);
+
+            // gesture dectections
+            if (hand.length > 0) {
+                const GE = new fp.GestureEstimator([
+                    fp.Gestures.VictoryGesture,
+                    fp.Gestures.ThumbsUpGesture,
+
+                ])
+
+                //estimate gesture
+                const gesture = await GE.estimate(hand[0].landmarks, 8)
+                console.log(gesture)
+                if ((gesture.gestures[0].name) !== undefined) {
+                    setRecGesture(gesture)
+                    console.log('111' ,gesture.gestures[0].name)
+                    console.log('2222',recGesture)
+                }
+            }
 
             // Draw mesh
             const ctx = canvasRef.current.getContext("2d");
@@ -83,6 +108,19 @@ function Hand() {
                     width: 640,
                     height: 480
                 }} />
+            <div className="grOutput" style={{
+                position: "relative",
+                marginLeft: "auto",
+                marginRight: "auto",
+                left: 400,
+                right: 0,
+                textAlign: "center",
+                zindex: 9,
+                width: 640,
+                height: 480
+            }}>
+                <h1> Gesture </h1>
+            </div>
         </div>
     )
 }
